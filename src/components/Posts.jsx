fd17446ed0db22f776f58623f9c429cf18b4e5ee
avu0s <avuwake@gmail.com>
2022-11-19 17:00:51 +0100

    Trimmed Components

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,6 +1,5 @@
 import data from "./data";
 import Post from "./Post";
-import Stack from "./Stack";
 
 function Posts() {
   const items = data.map((item) => <Post key={item.id} {...item} />);
--- src/components/Stack.jsx
+++
@@ -1,8 +0,0 @@
-import data from "./data";
-function Stack() {
-  const items = data.tools.map((item) => <img src={item} />);
-  return <>{items}</>;
-  console.log(items);
-}
-
-export default Stack;
